2db6483cdd9c5e1a4b5475ee7f92fe2e21d6aa26
Sravan T P <sravantp03@gmail.com>
2023-01-04 20:34:23 +0530

    added Plans component

---
+++ src/components/Plans/Plans.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Plans.css';
+import uuid from 'react-uuid';
+import { plansData } from '../../data/plansData';
+import whiteTick from '../../assets/whiteTick.png';
+import Rightarrow from '../../assets/rightArrow.png';
+
+const Plans = () => {
+  return (
+    <div className="plans-container">
+        {/* header sections */}
+        <div className="program-header" style={{gap : '2rem'}}>
+            <span className='stroke-text'>ready to start</span>
+            <span>your journey</span>
+            <span className='stroke-text'>now withus</span>
+        </div>
+
+        {/* plans card section */}
+        <div className='plans'>
+            {
+                plansData.map((plan) => (
+                    <div className="plan" key={uuid()}>
+                        {plan.icon}
+                        <span>{plan.name}</span>
+                        <span>$ {plan.price}</span>
+
+                        {/* features section */}
+                        <div className="features">
+                            {
+                                plan.features.map((feature) => (
+                                    <div className="feature" key={uuid()}>
+                                        <img src={whiteTick} alt="tick" />
+                                        <span>{feature}</span>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <div className='benifit-sec'>
+                            <span>See more benifits</span>
+                            <img src={Rightarrow} alt="arrow" />
+                        </div>
+                        <button className='btn'>Join Now</button>
+                    </div>
+                ))
+            }
+        </div>
+    </div>
+  );
+}
+
+export default Plans;
