606d6a2cde732aee17435b6ffa9b57e4f8ff2587
Sravan T P <sravantp03@gmail.com>
2023-01-04 16:03:44 +0530

    added Programs component

---
+++ src/components/Programs/Programs.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Programs.css';
+import { programsData } from '../../data/programsData';
+import Rightarrow from '../../assets/rightArrow.png'
+
+const Programs = () => {
+  return (
+    <div className='Programs' id='programs'>
+        {/* header sections */}
+        <div className="program-header">
+            <span className='stroke-text'>Explore our</span>
+            <span>Programs</span>
+            <span className='stroke-text'>to shape you</span>
+        </div>
+
+        {/* programs details */}
+        <div className="program-categories">
+            {
+                programsData.map((program) => (
+                    <div className="category">
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+                        <div className="join-now">
+                            <span>Join Now</span>
+                            <img src={Rightarrow} alt="Join Now" />
+                        </div>
+                    </div>
+                ) )
+            }
+        </div>
+    </div>
+  );
+}
+
+export default Programs;
