ba4d3ef1f2477e569d47695fd0af1051a9b2cf42
Sravan T P <sravantp03@gmail.com>
2023-01-05 13:18:32 +0530

    added Footer Component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './Footer.css';
+import github from '../../assets/github.png';
+import instagram from '../../assets/instagram.png';
+import linkedin from '../../assets/linkedin.png'; 
+import logo from '../../assets/logo.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="Footer">
+        <hr />
+        <div className="social">
+            <img src={github} alt="github" />
+            <img src={instagram} alt="instagram" />
+            <img src={linkedin} alt="linkedin" />
+        </div>
+        <div className="footer-img">
+            <img src={logo} alt="logo" />
+        </div>
+    </div>
   );
 }
 
