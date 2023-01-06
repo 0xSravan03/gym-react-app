df987f75e8124fedf09aeeafa028bcf73bd0051a
Sravan T P <sravantp03@gmail.com>
2023-01-06 11:23:06 +0530

    added hamburger menu for mobile view

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './Header.css';
 import Logo from '../../assets/logo.png';
+import bars from '../../assets/bars.png';
+import { useState } from 'react';
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
   return (
     <div className='header'>
-        <img src={ Logo } alt="Logo" className='logo' />
-        <ul className='header-menu'>
-            <li>Home</li>
-            <li>Programs</li>
-            <li>Why us</li>
-            <li>Plans</li>
-            <li>Testimonials</li>
+      <img src={Logo} alt="Logo" className='logo' />
+      {menuOpened === false && mobile === true ? <img src={bars} alt="hamburger" style={{ width: '1.5rem', height: '1.5rem', backgroundColor: 'var(--appColor)', padding: '0.5rem', borderRadius: '5px' }} onClick={() => setMenuOpened(true)}/> : (
+        <ul className='header-menu' onClick={() => setMenuOpened(false)}>
+          <li>Home</li>
+          <li>Programs</li>
+          <li>Why us</li>
+          <li>Plans</li>
+          <li>Testimonials</li>
         </ul>
+      )}
     </div>
   );
 }
